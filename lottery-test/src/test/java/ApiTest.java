87e006e974faae0e74d3f294d307677ab527d49e
Linyu Zhao <linyuzhao0411@gmail.com>
2025-11-03 23:51:24 +0100

    完成测试

---
+++ lottery-test/src/test/java/ApiTest.java
@@ -0,0 +1,33 @@
+import cn.itedus.lottery.rpc.IActivityBooth;
+import cn.itedus.lottery.rpc.req.ActivityReq;
+import cn.itedus.lottery.rpc.res.ActivityRes;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.dubbo.common.logger.LoggerFactory;
+import org.apache.dubbo.config.annotation.Reference;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.logging.Logger;
+
+import org.apache.dubbo.config.annotation.Service;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+
+    private Logger logger = (Logger) LoggerFactory.getLogger(ApiTest.class);
+
+    @Reference(interfaceClass = IActivityBooth.class, url = "dubbo://127.0.0.1:20880")
+    private IActivityBooth activityBooth;
+
+    @Test
+    public void test_rpc() {
+        ActivityReq req = new ActivityReq();
+        req.setActivityId(100002L);
+        ActivityRes result = activityBooth.queryActivityById(req);
+        System.out.println(result);
+    }
+
+}
