0fd131fc7e03d2e9cb552b552e5308e67a071ba0
Linyu Zhao <linyuzhao0411@gmail.com>
2025-11-05 22:44:11 +0100

    finish award domain part

--- lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/vo/DrawAwardInfo.java
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/vo/DrawAwardInfo.java
@@ -1,4 +1,4 @@
-package cn.itedus.lottery.domain.strategy.model.vo;
+package cn.itedus.lottery.domain.strategy.vo;
 
 /**
  * @description: 中奖奖品信息
@@ -31,7 +31,7 @@ public class DrawAwardInfo {
     public DrawAwardInfo() {
     }
 
-    public DrawAwardInfo(String awardId, Integer awardType, String awardName, String awardContent) {
+    public DrawAwardInfo(String awardId, Integer awardType, String awardName,String awardContent) {
         this.awardId = awardId;
         this.awardType = awardType;
         this.awardName = awardName;
